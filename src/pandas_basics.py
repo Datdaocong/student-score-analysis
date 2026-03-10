c88bfdb4ae9181b55c5da1c8682309189d8d03d0
Dao Cong Dat <datdc.b25tv081@stu.ptit.edu.vn>
2026-03-10 23:11:09 +0700

    add studenr grade classification

--- src/pandas_basics.py
+++ src/pandas_basics.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv("data/students.csv")
 
-print(df)
+'''print(df)
 print(df.head())
 
 print(df.info())
@@ -10,4 +10,16 @@ print(df.info())
 print(df.describe())
 
 df['average'] = (df['math'] + df['english'] + df['science']) / 3
+print(df)'''
+
+def classify(score):
+    if score >= 8:
+        return "Excellent"
+    elif score >= 6.5:
+        return "Good"
+    else:
+        return "Average"
+
+df["grade"] = df["average"].apply(classify)
+
 print(df)
