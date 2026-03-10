884bfd35e09e7a290ee21a4b344a02816f3d8041
Dao Cong Dat <datdc.b25tv081@stu.ptit.edu.vn>
2026-03-10 23:02:36 +0700

    add numpy basic example

---
+++ src/numpy_basics.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+scores = np.array([8, 6, 9, 5, 7])
+
+print("Scores:", scores)
+
+print("Average:", np.mean(scores))
+print("Max:", np.max(scores))
+print("Min:", np.min(scores))
